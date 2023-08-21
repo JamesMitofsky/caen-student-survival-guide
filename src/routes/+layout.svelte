b5d12d854c39598628e894950ac24ac8709d228a
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-21 14:52:11 +0200

    Correct the order of truth evaluation for loading Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,11 @@
 
 		const hostname = location.hostname;
 
-		if (hostname !== 'localhost') {
+		if (hostname.startsWith('deploy-preview') || hostname.includes('--')) {
+			console.log('Deployment preview on Netlify: skipped running Google Analytics.');
+		} else if (hostname === 'localhost') {
+			console.log('Local development: skipped running Google Analytics.');
+		} else {
 			const script = document.createElement('script');
 			script.async = true;
 			script.src = 'https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL';
@@ -37,10 +41,6 @@
 				gtag('config', 'G-54Q685VHKL');
 			};
 			document.head.appendChild(script);
-		} else if (hostname.startsWith('deploy-preview') || hostname.includes('--')) {
-			console.log('Deployment preview on Netlify: skipped running Google Analytics.');
-		} else {
-			console.log('Local development: skipped running Google Analytics.');
 		}
 	});
 </script>
