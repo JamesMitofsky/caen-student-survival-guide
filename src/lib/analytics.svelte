fe820b13892f9f2b07da88418cfdecb3a658c9bf
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-21 14:35:36 +0200

    Deploy Google Analytics with catch statements to prevent data collection during development

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,29 @@
+<!-- Following this: https://joyofcode.xyz/sveltekit-google-analytics -->
+
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	$: {
+		if (typeof gtag !== 'undefined') {
+			gtag('config', 'G-54Q685VHKL', {
+				page_title: document.title,
+				page_path: $page.url.pathname
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL">
+	</script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+		gtag('config', 'G-54Q685VHKL');
+	</script>
+</svelte:head>
