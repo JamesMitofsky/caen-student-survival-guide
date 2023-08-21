eee72365fdd2c9524799b09fa0b7c8752dd441b7
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-21 10:55:00 +0200

    Inital commit of Decap CMS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,27 @@
 <script>
 	import '../app.css';
+
+	function initializeCms() {
+		//@ts-ignore
+		if (window?.netlifyIdentity) {
+			//@ts-ignore
+			window.netlifyIdentity.on('init', (user) => {
+				if (!user) {
+					//@ts-ignore
+					window.netlifyIdentity.on('login', () => {
+						document.location.href = '/admin/';
+					});
+				}
+			});
+		}
+	}
 </script>
+
+<svelte:head>
+	<script
+		src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+		on:load={initializeCms}
+	></script>
+</svelte:head>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script>
+	// Following these instructions: https://decapcms.org/docs/add-to-your-site/
+	function runNetlifyIdentity() {
+		// @ts-ignore
+		if (window.netlifyIdentity) {
+			// @ts-ignore
+			window.netlifyIdentity.on('init', (user) => {
+				if (!user) {
+					// @ts-ignore
+					window.netlifyIdentity.on('login', () => {
+						document.location.href = '/admin/';
+					});
+				}
+			});
+		}
+	}
+</script>
+
+<svelte:head>
+	<script
+		src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+		on:load={runNetlifyIdentity}
+	></script>
+</svelte:head>
+
 <h1 class="text-3xl font-bold underline">Hello Tailwind!</h1>
 <h2>Welcome to SvelteKit</h2>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
