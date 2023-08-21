4d44882a44e04f564dec1c97de63e99b1a3dcbe0
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-21 14:35:36 +0200

    Deploy Google Analytics with catch statements to prevent data collection during development

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
 
 	function initializeCms() {
@@ -15,6 +16,33 @@
 			});
 		}
 	}
+
+	onMount(() => {
+		//  Google tag (gtag.js) - Google Analytics
+
+		const hostname = location.hostname;
+
+		if (hostname !== 'localhost') {
+			const script = document.createElement('script');
+			script.async = true;
+			script.src = 'https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL';
+			script.onload = () => {
+				// @ts-ignore
+				window.dataLayer = window.dataLayer || [];
+				function gtag() {
+					// @ts-ignore
+					dataLayer.push(arguments);
+				}
+				gtag('js', new Date());
+				gtag('config', 'G-54Q685VHKL');
+			};
+			document.head.appendChild(script);
+		} else if (hostname.startsWith('deploy-preview') || hostname.includes('--')) {
+			console.log('Deployment preview on Netlify: skipped running Google Analytics.');
+		} else {
+			console.log('Local development: skipped running Google Analytics.');
+		}
+	});
 </script>
 
 <svelte:head>
