30841463a42c080725b567fd6930315ece099be8
James Mitofsky <JamesMitofsky@gmail.com>
2023-08-21 15:38:55 +0200

    No fancy catching to prevent bad tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,62 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
 	import '../app.css';
 
-	onMount(() => {
-		runGoogleAnalytics();
-	});
+	// onMount(() => {
+	// 	runGoogleAnalytics();
+	// });
 
-	async function runGoogleAnalytics() {
-		//  Google tag (gtag.js) - Google Analytics
+	// async function runGoogleAnalytics() {
+	// 	//  Google tag (gtag.js) - Google Analytics
 
-		const hostname = location.hostname;
+	// 	const hostname = location.hostname;
 
-		if (hostname.startsWith('deploy-preview') || hostname.includes('--')) {
-			console.log('Deployment preview on Netlify: skipped running Google Analytics.');
-		} else if (hostname === 'localhost') {
-			console.log('Local development: skipped running Google Analytics.');
-		} else {
-			const analyticsScript = await loadScript(
-				'https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL'
-			);
+	// 	if (hostname.startsWith('deploy-preview') || hostname.includes('--')) {
+	// 		console.log('Deployment preview on Netlify: skipped running Google Analytics.');
+	// 	} else if (hostname === 'localhost') {
+	// 		console.log('Local development: skipped running Google Analytics.');
+	// 	} else {
+	// 		const analyticsScript = await loadScript(
+	// 			'https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL'
+	// 		);
 
-			// @ts-ignore
-			window.dataLayer = window.dataLayer || [];
-			function gtag() {
-				// @ts-ignore
-				dataLayer.push(arguments);
-			}
-			// @ts-ignore
-			gtag('js', new Date());
-			// @ts-ignore
-			gtag('config', 'G-54Q685VHKL');
-		}
-	}
+	// 		// @ts-ignore
+	// 		window.dataLayer = window.dataLayer || [];
+	// 		function gtag() {
+	// 			// @ts-ignore
+	// 			dataLayer.push(arguments);
+	// 		}
+	// 		// @ts-ignore
+	// 		gtag('js', new Date());
+	// 		// @ts-ignore
+	// 		gtag('config', 'G-54Q685VHKL');
+	// 	}
+	// }
 
-	function loadScript(src: string) {
-		return new Promise((resolve, reject) => {
-			const script = document.createElement('script');
-			script.async = true;
-			script.src = src;
+	// function loadScript(src: string) {
+	// 	return new Promise((resolve, reject) => {
+	// 		const script = document.createElement('script');
+	// 		script.async = true;
+	// 		script.src = src;
 
-			document.body.appendChild(script);
+	// 		document.body.appendChild(script);
 
-			script.addEventListener('load', () => resolve(script));
-			script.addEventListener('error', () => reject(script));
-		});
-	}
+	// 		script.addEventListener('load', () => resolve(script));
+	// 		script.addEventListener('error', () => reject(script));
+	// 	});
+	// }
 </script>
+
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-54Q685VHKL"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-54Q685VHKL');
+	</script>
+</svelte:head>
 
 <slot />
